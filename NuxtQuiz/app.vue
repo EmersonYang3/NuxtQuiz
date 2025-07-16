3b062df411391b74e22fd30c06b70e0903037efa
EmersonYang3 <emersonyang28@gmail.com>
2025-07-16 08:04:23 -0400

    API reference

--- NuxtQuiz/app.vue
+++ NuxtQuiz/app.vue
@@ -1,1 +1,12 @@
 <template></template>
+
+<script lang="ts" setup>
+const { data, pending, error, refresh }: any = await useFetch(
+  "http://127.0.0.1:8000/api/quiz/1",
+  {}
+);
+
+console.log("yeah");
+console.log(data.value);
+console.log("after");
+</script>
