ea029ada109cbd3ffdc3b13a4be8f18f86fbfddc
Eric Chen <ericc404@nycstudents.net>
2025-07-17 10:19:32 -0400

    Refactor QuizCard and QuizContainer components for improved layout and functionality

--- NuxtQuiz/app.vue
+++ NuxtQuiz/app.vue
@@ -8,4 +8,20 @@
     <QuestionCard />
     <TheFooter />
   </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+  <QuizCard />
+  <QuestionCard />
 </template>
+
+<script lang="ts" setup>
+const { data, pending, error, refresh }: any = await useFetch(
+  "http://127.0.0.1:8000/api/quiz/1",
+  {}
+);
+
+console.log("yeah");
+console.log(data.value);
+console.log("after");
+</script>
